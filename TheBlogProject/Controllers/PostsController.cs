56d62e6e4dcf51b535d6e2348ffb02e6328cd326
Saeed Adam <sandeed70@gmail.com>
2022-07-10 08:59:58 +0300

    Removed Tag Contoller and View

--- TheBlogProject/Controllers/PostsController.cs
+++ TheBlogProject/Controllers/PostsController.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Npgsql.PostgresTypes;
 using TheBlogProject.Data;
 using TheBlogProject.Models;
 
